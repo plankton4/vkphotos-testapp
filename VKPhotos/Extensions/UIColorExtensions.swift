753d849eae271f160faa96ccc8b51a447a687763
Dmitry Iv <plankton4.rus@gmail.com>
2021-08-07 03:01:30 +0800

    reversedSystemColor для поддержки темной темы.

---
+++ VKPhotos/Extensions/UIColorExtensions.swift
@@ -0,0 +1,13 @@
+//
+//  UIColorExtensions.swift
+//  VKPhotos
+//
+//  Created by Dmitry Iv on 07.08.2021.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let reversedSystemColor = UIColor(named: "ReversedSystemColor")
+}
